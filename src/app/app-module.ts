d4eed4136713ca7ecb371d378af41e98286959f2
Nickolas Machado <nickolasraphael3232@gmail.com>
2025-09-18 12:53:10 -0300

    Bootstrap Installed

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { App } from './app';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [
     provideBrowserGlobalErrorListeners()
